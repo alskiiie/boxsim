bcbb9ad9d4dda7d8b5c05b28bf92b4da8806728e
alskiiie <39855812+alskiiie@users.noreply.github.com>
2018-07-27 18:53:06 +0200

    move

---
+++ Assets/rotation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour {
+
+
+    public static float speed = 1.2f;
+    public bool reverse = false;
+    public float personalSpeed;
+
+    // Use this for initialization
+    void Start () {
+            
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        personalSpeed = speed;
+        if (reverse)
+        {
+            personalSpeed *= -1;
+        }
+
+
+    transform.Rotate(new Vector3(0f, 0f, personalSpeed));
+	}
+
+    public void AdjustSpeed (float newspeed)
+    {
+        speed = newspeed;
+    }
+}
---
+++ Assets/spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour {
+
+    public float startTime;
+    public float Interval;
+
+    public Rigidbody2D traveler;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Create", startTime, Interval);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Create()
+    {
+        Instantiate(traveler, new Vector3(-6.5f, 5.5f, 0), Quaternion.identity);
+    }
+}
